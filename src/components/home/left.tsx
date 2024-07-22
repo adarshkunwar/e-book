7ebd253a88a29c5c55ce6f988262c7cb99f73988
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-07-22 13:49:47 +0545

    Card is more aligned and the section is divided into left and right

--- src/components/home/left.tsx
+++ src/components/home/left.tsx
@@ -26,7 +26,7 @@ export default async function Left() {
       {/* Last Reading Section */}
       <section>
         <h2 className="text-2xl font-bold mb-4">Last Reading</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <div className="grid grid-cols-4 sm:grid-cols-5 gap-4">
           {data.map((ebook: TBook, index: number) => (
             <SingleCardItem key={index} product={ebook} />
           ))}
--- src/components/shared-components/single-card-item.tsx
+++ src/components/shared-components/single-card-item.tsx
@@ -1,35 +1,37 @@
 import Image from "next/image";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { TBook } from "@/types/book";
-import { Button } from "../ui/button";
 import Link from "next/link";
 
 
 type Props = {
   product: TBook;
 };
+
+
 export default function SingleCardItem({ product }: Props) {
+  const upperCaseName = (name: string) => {
+    return name.charAt(0).toUpperCase() + name.slice(1);
+  }
+
   return (
     <Link href={`/api/books/${product.id}`}>
       <Card>
         <CardHeader>
-          <div className="aspect-square overflow-clip w-full">
+          <div className="aspect-[3/4] overflow-clip w-full p-2">
             <Image
               width={0}
               height={0}
               sizes="100vw"
-              className="h-full w-full rounded-lg object-cover"
+              className="h-full w-full rounded-lg object-cover shadow"
               src={product.cover}
               alt={product.title}
             />
           </div>
         </CardHeader>
         <CardContent>
-          <h2 className="text-sm">{product.genres.join(", ")}</h2>
-          <hr />
-          <h3>{product.title}</h3>
-          <h3>{product.author}</h3>
-          <Button> Read </Button>
+          <h3 className="text-lg font-black">{upperCaseName(product.title)}</h3>
+          <h3 className="text-sm">{product.author}</h3>
         </CardContent>
       </Card>
     </Link>
