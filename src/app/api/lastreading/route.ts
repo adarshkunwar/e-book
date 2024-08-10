d2a9ad1dad3c523bda82a10c73ccaf184ef3c749
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-08-10 07:58:52 +0545

    Added Last reading

---
+++ src/app/api/lastreading/route.ts
@@ -0,0 +1,66 @@
+import { books as Book } from "@/data/booksCollection";
+import { TBook, TLastReadingBook } from "@/types/book";
+import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from "next/server";
+// import prisma from '@/lib/prisma';
+
+export async function GET(req: NextRequest) {
+	///////////////////////////////////////////
+	// in case of using a backend
+	//
+	//
+	// const { userId } = req.query;
+	//
+	// if (typeof userId !== 'string') {
+	// 	return res.status(400).json({ error: 'Invalid user ID' });
+	// }
+	// try {
+	// 	const lastReadingBook = await prisma.book.findFirst({
+	// 		where: {
+	// 			// Example: Assuming each user has a `lastReadingBookId` in their User model
+	// 			users: {
+	// 				some: { id: userId },
+	// 			},
+	// 		},
+	// 		select: {
+	// 			id: true,
+	// 			title: true,
+	// 			currentChapter: true,
+	// 			totalChapter: true,
+	// 			author: true,
+	// 			cover: true,
+	// 			description: true,
+	// 		},
+	// 	});
+	//
+	// 	if (!lastReadingBook) {
+	// 		return res.status(404).json({ error: 'Last reading book not found' });
+	// 	}
+	//
+	// 	return res.status(200).json(lastReadingBook);
+	// } catch (error) {
+	// 	return res.status(500).json({ error: 'Internal server error' });
+	// }
+
+	///////////////////////////////////////////
+	// in case of using a local data
+	const lastReadingBook: TLastReadingBook = Book(1).map((book: TBook) => {
+		return {
+			id: book.id,
+			title: book.title,
+			currentChapter: book.currentChapter,
+			totalChapter: book.totalChapter,
+			author: book.author,
+			cover: book.cover,
+			summary: book.summary,
+		};
+
+	})[0];
+
+
+	const returable = NextResponse.json(lastReadingBook);
+	return returable;
+
+
+
+}
