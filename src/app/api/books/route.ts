87c896e63bd9cd989776866213adceaa111e1b9f
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-08-10 06:42:14 +0545

    Changed books to reflect a json data

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -3,7 +3,16 @@ import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const users = await prisma.book.findMany();
-  return NextResponse.json(users);
+
+  ///////////////////////////////////////////
+  // in case of using a backend 
+  // const users = await prisma.book.findMany();
+  // const returnable = NextResponse.json(users);
+
+  ///////////////////////////////////////////
+  // in case of using a local data
+  const returnable = NextResponse.json(books(10));
+
+  return returnable;
 }
 
