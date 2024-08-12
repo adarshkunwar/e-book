b5a6b0ce3e62630c2f59850d4eb561e7ce8f815b
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-08-12 08:56:57 +0545

    Single Book Api

---
+++ src/app/api/books/[id]/route.ts
@@ -0,0 +1,35 @@
+import { books as Book } from "@/data/booksCollection";
+import { TBook, TLastReadingBook } from "@/types/book";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+  ///////////////////////////////////////////
+  // slug 
+  const { id } = params;
+  if (isNaN(Number(id))) {
+    return NextResponse.json({ error: 'Invalid book ID' }, { status: 400 });
+  }
+
+  ///////////////////////////////////////////
+  // in case of using a local data
+  const selectedBook = Book(100).find(item => item.id == id);
+  if (!selectedBook) {
+    return NextResponse.json({ error: 'Book not found' }, { status: 404 });
+  }
+
+  const bookData: TBook = {
+    id: selectedBook.id,
+    title: selectedBook.title,
+    currentChapter: selectedBook.currentChapter,
+    totalChapter: selectedBook.totalChapter,
+    author: selectedBook.author,
+    cover: selectedBook.cover,
+    summary: selectedBook.summary,
+    published: selectedBook.published,
+    genres: selectedBook.genres,
+    description: selectedBook.description,
+  }
+
+  const returable = NextResponse.json(bookData, { status: 200 });
+  return returable;
+}
