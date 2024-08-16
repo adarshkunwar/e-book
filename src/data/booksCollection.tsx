dcabb597b4c74d7f61af8ab3a5075329b7340e67
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-08-16 23:02:16 +0545

    Added chapters to fake json

--- src/data/booksCollection.tsx
+++ src/data/booksCollection.tsx
@@ -1,5 +1,6 @@
 import { faker } from "@faker-js/faker";
 import { TBook } from "@/types/book";
+import { TChapter } from '@/types/chapter';
 
 const genres = [
   "Fiction",
@@ -61,9 +62,25 @@ const genres = [
 const randomNumber = (min: number, max: number) =>
   Math.floor(Math.random() * (max - min) + min);
 
+const chapters = (book_id: string): TChapter[] => {
+  const chapters: TChapter[] = [];
+  const chapterCount = randomNumber(4, 10);
+  for (let i = 0; i < chapterCount; i++) {
+    chapters.push({
+      id: (i + 1).toString(),
+      book_id: book_id,
+      title: faker.lorem.words(),
+      content: faker.lorem.paragraphs(50),
+      tag_line: faker.lorem.sentence()
+    })
+  }
+  return chapters;
+}
+
 export const books = (number: number): TBook[] => {
   const array = [];
   for (let i = 0; i < number; i++) {
+    const chapterData = chapters((i + 1).toString())
     array.push({
       id: (i + 1).toString(),
       title: faker.lorem.words(),
@@ -72,12 +89,13 @@ export const books = (number: number): TBook[] => {
       description: faker.lorem.paragraphs(25),
       summary: faker.lorem.paragraph(5),
       published: faker.date.past().toISOString(),
+      chapters: chapterData,
       genres: Array.from(
         { length: 3 },
         () => genres[randomNumber(0, genres.length)]
       ),
-      currentChapter: randomNumber(1, 10),
-      totalChapter: randomNumber(10, 100),
+      currentChapter: randomNumber(1, chapterData.length),
+      totalChapter: chapterData.length,
     });
   }
   return array;
