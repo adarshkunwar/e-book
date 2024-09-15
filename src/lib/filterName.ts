7346ac2262812ec2d366e8b7362d6131f645dd2f
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-09-16 00:45:05 +0545

    Checks if the text sent to filterName is string or not

--- src/lib/filterName.ts
+++ src/lib/filterName.ts
@@ -1,16 +1,21 @@
 export const Capitalize = (input: string) => {
-  const split = input.split(' ');
-  const capitalisingEachWord = split.map((word) => { return word.charAt(0).toUpperCase() + word.slice(1) });
-  const returnable = capitalisingEachWord.join(' ');
+  if (typeof input !== "string") {
+    return "";
+  }
+  const split = input.split(" ");
+  const capitalisingEachWord = split.map((word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  });
+  const returnable = capitalisingEachWord.join(" ");
   return returnable;
 };
 
 export const RemoveSpecialCharacters = (input: string) => {
-  const returnable = input.replace(/[^\w\s]/gi, '');
-  return returnable
-}
+  const returnable = input.replace(/[^\w\s]/gi, "");
+  return returnable;
+};
 
 export const RemoveMultipleSpaces = (input: string) => {
-  const returnable = input.replace(/\s+/g, ' ');
-  return returnable
-}
+  const returnable = input.replace(/\s+/g, " ");
+  return returnable;
+};
