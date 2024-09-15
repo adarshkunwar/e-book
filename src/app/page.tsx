77a08ac2329d06458afbe02f8fa82814759fa29b
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-09-15 20:36:13 +0545

    Home Page is rendered here

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Home from "@/app/(common)/home/page";
+import SideBarFinal from "@/components/shared-components/layout/sidebar";
+
+export default function Page() {
+  return (
+    <SideBarFinal>
+      <Home />
+    </SideBarFinal>
+  );
+}
