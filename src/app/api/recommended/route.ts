12d81ef234c5edfcca995b5033b026980446c8bc
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-09-16 01:08:45 +0545

    Books are recommended without any rhyme or reason

--- src/app/api/recommended/route.ts
+++ src/app/api/recommended/route.ts
@@ -1,27 +1,35 @@
-import { books as Book } from "@/data/booksCollection";
 import prisma from "@/lib/prisma";
-import { TBook, TBookCard } from "@/types/book";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-
   ///////////////////////////////////////////
-  // in case of using a backend 
-  // const users = await prisma.book.findMany();
-  // const returnable = NextResponse.json(users);
+  // in case of using a backend
+  // TODO: Book recommendation logic
+  const books = await prisma.book.findMany({
+    include: {
+      author: {
+        select: {
+          id: true,
+          firstName: true,
+          lastName: true,
+          email: true,
+        },
+      },
+    },
+  });
+  const returnable = NextResponse.json(books);
 
   ///////////////////////////////////////////
   // in case of using a local data
-  const booksCollections: TBookCard[] = Book(4).map((book: TBook) => {
-    return {
-      id: book.id,
-      title: book.title,
-      author: book.author,
-      cover: book.cover
-    };
-  });
-  const returnable = NextResponse.json(booksCollections);
+  // const booksCollections: TBookCard[] = Book(4).map((book: TBook) => {
+  //   return {
+  //     id: book.id,
+  //     title: book.title,
+  //     author: book.author,
+  //     cover: book.cover
+  //   };
+  // });
+  // const returnable = NextResponse.json(booksCollections);
 
   return returnable;
 }
-
